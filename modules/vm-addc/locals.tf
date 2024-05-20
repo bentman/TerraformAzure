179b80aac2a7999201e14554fa0c601d220593d1
bentman <26147320+bentman@users.noreply.github.com>
2024-05-20 05:30:22 -0500

    learning child modules

--- modules/vm-addc/locals.tf
+++ modules/vm-addc/locals.tf
@@ -7,16 +7,17 @@ locals {
   servers_ou_path = "OU=Servers,${join(",", [for dc in local.split_domain : "DC=${dc}"])}"
 
   # Generate commands to install DNS and AD Forest
-  powershell_gpmc = [
-    "New-Item -Path 'c:\\BUILD\\' -ItemType Directory -Force -ea 0",
-    "Disable-WindowsOptionalFeature -Online -FeatureName AzureArcSetup -LogPath 'c:\\BUILD\\disableAzureArcSetup.log' -Verbose",
-    "$safePswd = ConvertTo-SecureString '${var.safemode_admin_pswd}' -AsPlainText -Force",
-    "Add-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools",
-    "Install-WindowsFeature DNS -IncludeAllSubFeature -IncludeManagementTools",
-    "Import-Module ADDSDeployment, DnsServer",
-    "Install-ADDSForest -DomainName '${var.domain_name}' -DomainNetBiosName '${var.domain_netbios_name}' -InstallDns -SafeModeAdministratorPassword $safePswd -NoRebootOnCompletion:$false -LogPath 'C:\\BUILD\\adpromo.log' -Confirm:$false -Force:$true -Verbose",
-    "exit 0",
-  ]
+  log_path = "New-Item -Path 'c:\\BUILD\\' -ItemType Directory -Force -ea 0"
+  arc_disa = "Disable-WindowsOptionalFeature -Online -FeatureName AzureArcSetup -LogPath 'c:\\BUILD\\disableAzureArcSetup.log' -Verbose"
+  pwd_safe = "$safePswd = ConvertTo-SecureString '${var.safemode_admin_pswd}' -AsPlainText -Force"
+  ins_adds = "Add-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools"
+  ins_dnss = "Install-WindowsFeature DNS -IncludeAllSubFeature -IncludeManagementTools"
+  imp_modu = "Import-Module ADDSDeployment, DnsServer"
+  dc_promo = "Install-ADDSForest -DomainName '${var.domain_name}' -DomainNetBiosName '${var.domain_netbios_name}' -InstallDns -SafeModeAdministratorPassword $safePswd -NoRebootOnCompletion:$false -LogPath 'C:\\BUILD\\adpromo.log' -Confirm:$false -Force:$true -Verbose"
+  exit_hck = "exit 0"
+
+  # PoSh commands list to pass to server over SSH
+  powershell_gpmc = "${local.log_path};${local.arc_disa};${local.pwd_safe};${local.ins_adds};${local.ins_dnss};${local.imp_modu};${local.dc_promo};${local.exit_hck};"
 }
 /*
 locals {
