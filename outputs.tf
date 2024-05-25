12ceda6074acf5ffb1c6570fb08478540559dc1e
bentman <26147320+bentman@users.noreply.github.com>
2024-05-25 14:06:35 -0500

    updates to comments/notes/instructions

--- outputs.tf
+++ outputs.tf
@@ -1,77 +1,88 @@
 #################### OUTPUTS ####################
 ##### main.tf outputs
+
 # Outputs the name of the resource group
 output "resource_group_name" {
-  value = azurerm_resource_group.mylab.name
+  description = "The name of the resource group"
+  value       = azurerm_resource_group.mylab.name
 }
 
 # Outputs the location of the resource group
 output "resource_group_location" {
-  value = azurerm_resource_group.mylab.location
+  description = "The location of the resource group"
+  value       = azurerm_resource_group.mylab.location
 }
 
 ##### v-network.tf outputs
+
 # Outputs the name of the virtual network
 output "lab_network_vnet_name" {
-  value = module.v_network.lab_network_vnet_name
+  description = "The name of the virtual network"
+  value       = module.v_network.lab_network_vnet_name
 }
 
 # Outputs the virtual network resource
 output "lab_network_vnet" {
-  value = module.v_network.lab_network_vnet
+  description = "The virtual network resource"
+  value       = module.v_network.lab_network_vnet
 }
 
 # Outputs the subnet resource
 output "lab_network_snet" {
-  value = module.v_network.lab_network_snet
+  description = "The subnet resource"
+  value       = module.v_network.lab_network_snet
 }
 
 # Outputs the public IP of the gateway
 output "lab_gw_pip" {
-  value = module.v_network.lab_gw_pip
+  description = "The public IP of the gateway"
+  value       = module.v_network.lab_gw_pip
 }
 
-##### vm-jumpWin OUTPUTS
+##### vm-jumpWin outputs
+
 # Outputs the public DNS name of the Windows jumpbox VM, if it exists
 output "vm_jumpwin_public_name" {
-  value       = length(module.vm_jumpbox) > 0 ? module.vm_jumpbox[0].vm_jumpwin_public_name : null
   description = "The public DNS name of Windows jumpbox VM, if exists - 'null' if not"
+  value       = length(module.vm_jumpbox) > 0 ? module.vm_jumpbox[0].vm_jumpwin_public_name : null
 }
 
 # Outputs the public IP address of the Windows jumpbox VM, if it exists
 output "vm_jumpwin_public_ip" {
-  value       = length(module.vm_jumpbox) > 0 ? module.vm_jumpbox[0].vm_jumpwin_public_ip : null
   description = "The public IP address of Windows jumpbox VM, if exists - 'null' if not"
+  value       = length(module.vm_jumpbox) > 0 ? module.vm_jumpbox[0].vm_jumpwin_public_ip : null
 }
 
-##### vm-jumpLin OUTPUTS
+##### vm-jumpLin outputs
+
 # Outputs the public DNS name of the Linux jumpbox VM, if it exists
 output "vm_jumplin_public_name" {
-  value       = length(module.vm_jumpbox) > 0 ? module.vm_jumpbox[0].vm_jumplin_public_name : null
   description = "The public DNS name of Linux jumpbox VM, if exists - 'null' if not"
+  value       = length(module.vm_jumpbox) > 0 ? module.vm_jumpbox[0].vm_jumplin_public_name : null
 }
 
 # Outputs the public IP address of the Linux jumpbox VM, if it exists
 output "vm_jumplin_public_ip" {
-  value       = length(module.vm_jumpbox) > 0 ? module.vm_jumpbox[0].vm_jumplin_public_ip : null
   description = "The public IP address of Linux jumpbox VM, if exists - 'null' if not"
+  value       = length(module.vm_jumpbox) > 0 ? module.vm_jumpbox[0].vm_jumplin_public_ip : null
 }
 
 ##### vm-addc.tf outputs
+
 # Outputs the public DNS name of the VM ADDC, if it exists
 output "vm_addc_public_name" {
-  value       = length(module.vm_addc) > 0 ? module.vm_addc[0].vm_addc_public_name : null
   description = "The public DNS name of VM ADDC, if exists"
+  value       = length(module.vm_addc) > 0 ? module.vm_addc[0].vm_addc_public_name : null
 }
 
 # Outputs the public IP address of the VM ADDC, if it exists
 output "vm_addc_public_ip" {
-  value       = length(module.vm_addc) > 0 ? module.vm_addc[0].vm_addc_public_ip : null
   description = "The public IP address of VM ADDC, if exists"
+  value       = length(module.vm_addc) > 0 ? module.vm_addc[0].vm_addc_public_ip : null
 }
 
 # Outputs a list of variables to verify on 'apply' - modify in vm-addc/outputs.tf
 output "addc_module_vars" {
-  value       = length(module.vm_addc) > 0 ? module.vm_addc[0].addc_module_vars : null
   description = "List of vars to verify on 'apply' - modify in vm-addc/outputs.tf"
+  value       = length(module.vm_addc) > 0 ? module.vm_addc[0].addc_module_vars : null
 }
