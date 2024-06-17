ecb401cb07d15daf7814d8e5cf5408b845af7d8b
bentman <26147320+bentman@users.noreply.github.com>
2024-06-17 03:35:50 -0500

    formatting and readability

--- modules/sql-ha/Add-Users.ps1
+++ modules/sql-ha/Add-Users.ps1
@@ -4,23 +4,74 @@ param (
     [Parameter(ValueFromPipeline = $true, Mandatory = $true)] [string]$sql_svc_acct_user,
     [Parameter(ValueFromPipeline = $true, Mandatory = $true)] [string]$sql_svc_acct_pswd
 )
-# Split the domain name into its components
+
+# Split domain name into its components
 $split_domain = $domain_name.Split(".")
-# Construct the DN path from the split domain components
+# Construct DN path from split domain components
 $dn_path = ($split_domain | ForEach-Object { "DC=$_" }) -join ","
-# Check if the directory 'C:\BUILD\Logs\' exists, and create it if it does not
-if (!(Test-Path -Path 'C:\BUILD\Logs\')) { New-Item -Path 'C:\BUILD\' -ItemType Directory -Force }
-# Start a transcript to log all activities to the specified path
+
+# Check if directory 'C:\BUILD\Logs\' exists, and create it if it does not
+if (!(Test-Path -Path 'C:\BUILD\Logs\')) { 
+    New-Item -Path 'C:\BUILD\Logs\' -ItemType Directory -Force 
+}
+
+# Start transcript to log all activities to specified path
 Start-Transcript -Path 'C:\BUILD\Logs\transcript-add_users.txt'
-# Import the Active Directory module to use AD-related cmdlets
+
+# Import Active Directory module to use AD-related cmdlets
 Import-Module ActiveDirectory
-# Create a new Organizational Unit (OU) named 'Servers' at the specified DN path
-New-ADOrganizationalUnit -Name 'Servers' -Path "$dn_path" -Description 'Servers OU for new objects' -Verbose
-# Create a new AD user for SQL installation with the specified details
-New-ADUser -Name 'sqlinstall' -GivenName 'SQL' -Surname 'INSTALL' -UserPrincipalName "sqlinstall@$domain_name" -SamAccountName 'sqlinstall' -AccountPassword (ConvertTo-SecureString "$sql_svc_acct_pswd" -AsPlainText -Force) -Enabled $true -Verbose
-# Add the newly created SQL install user to the 'Domain Admins' group
-Add-ADGroupMember -Identity 'Domain Admins' -Members sqlinstall
-# Create a new AD user for SQL service account with the specified details
-New-ADUser -Name "$sql_svc_acct_user" -GivenName 'SQL' -Surname 'SERVICE' -UserPrincipalName "$sql_svc_acct_user@$domain_name" -SamAccountName $sql_svc_acct_user -AccountPassword (ConvertTo-SecureString "$sql_svc_acct_pswd" -AsPlainText -Force) -Enabled $true -Verbose
-# Stop the transcript
+
+# Create new Organizational Unit (OU) named 'Servers' at specified DN path if not exist
+if (-not (Get-ADOrganizationalUnit -Filter "Name -eq 'Servers'")) {
+    New-ADOrganizationalUnit -Name 'Servers' -Path "$dn_path" -Description 'OU for Server objects' -Verbose
+}
+
+# Create new AD user for SQL installation with specified details
+New-ADUser `
+    -SamAccountName 'sqlinstall' `
+    -Name 'sqlinstall' `
+    -GivenName 'SQL' `
+    -Surname 'INSTALL' `
+    -UserPrincipalName "sqlinstall@$domain_name" `
+    -AccountPassword (ConvertTo-SecureString "$sql_svc_acct_pswd" -AsPlainText -Force) `
+    -Enabled $true `
+    -Verbose
+
+# Set password options and other properties
+Set-ADUser -Identity 'sqlinstall' `
+    -PasswordNeverExpires $true `
+    -ChangePasswordAtLogon $false `
+    -CannotChangePassword $true `
+    -Description 'SQL Install Account' `
+    -DisplayName 'SQL Install Account'
+
+# Add newly created SQL install user to 'Domain Admins' group (more permission than required)
+Add-ADGroupMember -Identity "Domain Admins" -Members 'sqlinstall'
+
+# Create new Organizational Unit (OU) named 'Service_Accounts' at specified DN path if not exist
+if (-not (Get-ADOrganizationalUnit -Filter "Name -eq 'Service_Accounts'")) {
+    New-ADOrganizationalUnit -Name 'Service_Accounts' -Path "$dn_path" -Description 'OU for Service_Accounts' -Verbose
+}
+
+# Create new AD user for SQL service account with specified details
+New-ADUser `
+    -SamAccountName $sql_svc_acct_user `
+    -Name "$sql_svc_acct_user" `
+    -GivenName 'SQL' `
+    -Surname 'SERVICE' `
+    -UserPrincipalName "$sql_svc_acct_user@$domain_name" `
+    -AccountPassword (ConvertTo-SecureString "$sql_svc_acct_pswd" -AsPlainText -Force) `
+    -Path "OU=Service_Accounts,$dn_path" `
+    -Enabled $true `
+    -Verbose
+
+# Set password options and other properties
+Set-ADUser -Identity $sql_svc_acct_user `
+    -PasswordNeverExpires $true `
+    -ChangePasswordAtLogon $false `
+    -CannotChangePassword $true `
+    -Description 'SQL Service Account' `
+    -DisplayName 'SQL Service Account'
+
+# Stop transcript
 Stop-Transcript
--- modules/sql-ha/Install-DomainController.ps1
+++ modules/sql-ha/Install-DomainController.ps1
@@ -1,18 +1,18 @@
 <#
 .SYNOPSIS
-    This script automates the setup and configuration of an Active Directory Domain Services (AD DS) environment on a Windows server.
+    This script automates setup and configuration of an Active Directory Domain Services (AD DS) environment on Windows server.
 .DESCRIPTION
-    The script installs and configures AD DS, DNS, and related features, sets up a new AD forest, adjusts firewall settings, and performs other necessary configurations.
+    The script installs and configures AD DS, DNS, and related features, sets up new AD forest, adjusts firewall settings, and performs other necessary configurations.
 .PARAMETER domain_name
-    (Mandatory) The fully qualified domain name (FQDN) for the new AD forest.
+    (Mandatory) The fully qualified domain name (FQDN) for new AD forest.
 .PARAMETER domain_netbios_name
-    (Mandatory) The NetBIOS name for the new AD forest.
+    (Mandatory) The NetBIOS name for new AD forest.
 .PARAMETER safemode_admin_pswd
-    (Mandatory) The password for the Directory Services Restore Mode (DSRM) administrator.
+    (Mandatory) The password for Directory Services Restore Mode (DSRM) administrator.
 .EXAMPLE
     .\YourScriptName.ps1 -domain_name "example.com" -domain_netbios_name "EXAMPLE" -safemode_admin_pswd "P@ssw0rd"
 .NOTES
-    Ensure that the script is run with administrative privileges.
+    Ensure that script is run with administrative privileges.
 #>
 
 [CmdletBinding()]
@@ -22,40 +22,40 @@ param (
     [Parameter(ValuefromPipeline=$true,Mandatory=$true)] [string]$safemode_admin_pswd
 )
 
-# Convert the safe mode administrator password to a secure string
+# Convert safe mode administrator password to secure string
 $safe_admin_pswd = ConvertTo-SecureString $safemode_admin_pswd -AsPlainText -Force
 # Create directory for logs if it doesn't already exist
 New-Item -Path 'c:\BUILD\Logs\' -ItemType Directory -Force -ea 0
 # Start transcript logging
 Start-Transcript -Path 'c:\BUILD\Logs\00-Provision.log'
-# Set the security protocol to TLS 1.2
+# Set security protocol to TLS 1.2
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-# Install the NuGet package provider
+# Install NuGet package provider
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-# Set PowerShell Gallery as a trusted repository
+# Set PowerShell Gallery as trusted repository
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 # Install necessary Windows features
 Install-WindowsFeature -Name AD-Domain-Services -IncludeAllSubFeature -Verbose
 Install-WindowsFeature -Name RSAT-AD-Tools -Verbose
-# Import the AD DS Deployment module
+# Import AD DS Deployment module
 Import-Module -Name ADDSDeployment -Verbose
 # Install DNS server features
 Install-WindowsFeature -Name DNS -IncludeAllSubFeature -Verbose
 Install-WindowsFeature -Name RSAT-DNS-Server -Verbose
-# Import the DNS Server module
+# Import DNS Server module
 Import-Module -Name DnsServer -Verbose
-# Install a new AD DS forest
+# Install new AD DS forest
 Install-ADDSForest -DomainName $domain_name -DomainNetBiosName $domain_netbios_name -InstallDns -SafeModeAdministratorPassword $safe_admin_pswd -NoRebootOnCompletion:$true -LogPath 'C:\BUILD\01-DCPromo.log' -Confirm:$false -Force -Verbose
 # Disable NLA for Terminal Server (RDP) user authentication setting
 Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name 'UserAuthentication' -Value 0
-# Disable the firewall for the Domain profile
+# Disable firewall for Domain profile
 Set-NetFirewallProfile -Profile Domain -Enabled:false
-# Create a firewall rule to allow SSH traffic
+# Create firewall rule to allow SSH traffic
 New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22 -Profile Any
-# Disable the Azure Arc Setup feature
+# Disable Azure Arc Setup feature
 Disable-WindowsOptionalFeature -Online -FeatureName AzureArcSetup -NoRestart -LogPath 'c:\BUILD\disableAzureArcSetup.log' -Verbose
 # Stop transcript logging
 Stop-Transcript
-# Exit the script (escaping possible errors for automation)
+# Exit script (escaping possible errors for automation)
 exit 0
 
